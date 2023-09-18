20d2d15bf5610f46d9dcf59bbb1498ef5ffad911
calvinseptyanto <98633109+calvinseptyanto@users.noreply.github.com>
2023-09-18 17:41:34 +0800

    Finished Landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,18 +1,13 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import { LandingNavbar } from "@/components/landing-navbar";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
   return (
-    <div>
-      LandingPage (Unprotected)
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link>
-      </div>
+    <div className="h-full ">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
